7ddf8c3be6f9c82d30c80fa33bcd67fc5323a3ce
Nazareno Alves da Costa <nazarenoalvesdacosta@gmail.com>
2023-06-23 11:21:45 -0300

    feat: impelmentação da lógica de pesquisa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 "use client"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import fetchApi from '@/services/fetchApi'
 
 export default function Home() {
-  useEffect(() => {
+  const [data, setData] = useState([])
+  const [search, setSearch] = useState('')
+
+  const handeClick = () => {
     fetchApi.get('characters', {
       params: {
-        nameStartsWith: 'Iron'
+        nameStartsWith: search
       }
     }).then(response => console.log(response.data)
     ).catch(err => console.log(err)
     )
-  },[])
+  }
   return (
+    <>
    <h1>Olá mundo</h1>
+   <input type='text' onChange={({ target }) => setSearch(target.value)}/>
+   <button type='button' onClick={handeClick}>Search</button>
+   </>
   )
 }
